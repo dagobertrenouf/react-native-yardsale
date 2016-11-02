2d3db4f51d2884a881b3d0d2672c763cb0ec5e55
Dagobert Renouf <dagobert.king@gmail.com>
2016-11-02 17:23:12 +0100

    Simple fetchMore config

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {
   ActivityIndicator,
   Text,
+  TouchableHighlight,
   View
 } from 'react-native';
 import { graphql } from 'react-apollo';
@@ -9,25 +10,52 @@ import gql from 'graphql-tag';
 
 class App extends Component {
   render() {
-    const { allTodoes, loading } = this.props.data;
+    const { allTodoes, loading, loadNextPage } = this.props;
     if (loading) {
       return (<ActivityIndicator />);
     }
     return (
       <View>
         { allTodoes.length && allTodoes.map((todo) => {
-          return <Text key={todo.id}>{todo.updatedAt}</Text>
+          return <Text key={todo.id}>{todo.name}</Text>
         })}
+        <TouchableHighlight onPress={loadNextPage}>
+          <Text>Load more</Text>
+        </TouchableHighlight>
       </View>
     );
   }
 }
 
-const MyQuery = gql`query MyQuery {
-  allTodoes {
-    id,
-    updatedAt
+const TodoesQuery = gql`
+  query Todoes($after: String) {
+    allTodoes(first: 3, after: $after) {
+      id,
+      name
+    }
   }
-}`;
+`;
 
-export default graphql(MyQuery)(App);
+const AppWithData = graphql(TodoesQuery, {
+  props({ data: { loading, allTodoes = [], fetchMore } }) {
+    return {
+      loading,
+      allTodoes,
+      loadNextPage() {
+        return fetchMore({
+          variables: {
+            after: allTodoes[allTodoes.length-1].id
+          },
+          updateQuery: (prev, { fetchMoreResult }) => {
+            if (!fetchMoreResult.data) { return prev; }
+            return Object.assign({}, prev, {
+              allTodoes: [...prev.allTodoes, ...fetchMoreResult.data.allTodoes],
+            });
+          },
+        })
+      }
+    }
+  }
+})(App);
+
+export default AppWithData;
